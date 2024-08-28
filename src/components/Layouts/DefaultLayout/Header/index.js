fe2e4af52cb9f619e8b9c5ed2fb0676242385ce6
Tuan3010 <tuanly.0234@gmail.com>
2024-08-28 17:10:52 +0700

    8/10/2024

--- src/components/Layouts/DefaultLayout/Header/index.js
+++ src/components/Layouts/DefaultLayout/Header/index.js
@@ -8,33 +8,95 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
 import { faCartShopping, faSearch, faUser, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 
 const Header = () => {
     console.log('re-rendering---');
     //Css cho Phần Cate OverPlay cho thiết bị mobile
-    
     const [isHovered, setIsHovered] = useState(false);
     return (
         <header className={clsx(style.mainHeader)}>
             <div className="grid wide">
                 <div className={clsx(style.containerMainHeader)}>
-                    <img className={style.logoHeader} src={logo} width={'80px'}/>
+
+                    <Link>
+                        <img className={style.logoHeader} src={logo} width={'80px'}/>
+                    </Link>
 
                     <div className={style.searchAndCate}>
-
+                        {/* Btn cate for PC */}
                         <div className={style.btnCate}
                              onMouseEnter={() => setIsHovered(true)}
                              onMouseLeave={() => setIsHovered(false)}
                         >
                             <FontAwesomeIcon icon={faBars} />                
-                            <span className={style.btnCateText}>Danh mục</span>               
+                            <span className={style.btnCateText}>Danh mục</span>
                             {isHovered && <CateHover />}                 
                         </div>
-
                         <div className={isHovered ? style.cateOverPlay : ''}></div>
-
+                        {/* Btn category for mobile */}
+                        <label for="nav-mobile-input" className={style.btnCateMobile}>
+                            <FontAwesomeIcon icon={faBars} />
+                        </label>
+                        <input hidden type="checkbox" id="nav-mobile-input" className={style.navMobileInput}></input>
+                        <div className={style.navCateMobileOverPlay}>
+                            <div className={style.navCateMobile}>
+                                <div className={style.navCateHeaderMobile}>
+                                    <div>
+                                        <img src={logo} width={'50px'} style={{marginTop: '2px', marginLeft: '4px'}} />
+                                    </div>
+                                    <label for='nav-mobile-input'><FontAwesomeIcon icon={faXmark} size="2xl" style={{color: '#fff', marginRight: '10px'}}/></label>
+                                </div>
+                                <div className={style.navContentMobile}>
+                                    <div className={style.navIntroduceMobile}>
+                                        <div className={style.navCateHeaderMobileTop}>
+                                            <div className={style.navIntroduceRight}>
+                                                <span className={style.navIntroduceRightText}>Đăng nhập hoặc đăng ký để nhận thêm nhiều ưu đãi hấp dẫn</span>   
+                                                <button className={style.btnSignUpMobile}>Đăng ký</button>
+                                                <button className={style.btnSignInMobile}>Đăng nhập</button>
+                                            </div>
+                                            <div className={style.navIntroduceRight}>
+                                                <img src={loginMobile} width={'126px'}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                    <div className={style.navItemCateMobileWrap}>
+                                        <ul style={{padding:'0', margin:'0'}}>
+                                            <li className={style.navItemCateMobile}>
+                                                <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
+                                            </li>
+                                            <li className={style.navItemCateMobile}>
+                                                <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className={style.navItemCateMobileWrap}>
+                                        <ul style={{padding:'0', margin:'0'}}>
+                                            <li className={style.navItemCateMobile}>
+                                                <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
+                                            </li>
+                                            <li className={style.navItemCateMobile}>
+                                                <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className={style.navItemCateMobileWrap}>
+                                        <ul style={{padding:'0', margin:'0'}}>
+                                            <li className={style.navItemCateMobile}>
+                                                <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
+                                            </li>
+                                            <li className={style.navItemCateMobile}>
+                                                <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                         <div className={style.searchInputContainer}>
                             <form className={clsx('flex')} style={{alignItems: 'center'}}  action="" method="">
                                 <input className={clsx(style.searchInput)} placeholder="Nhập tên cần tìm ..."></input>
@@ -62,61 +124,7 @@ const Header = () => {
                 </div>
             </div>
             
-            <div className={style.navCateMobileOverPlay}>
-                <div className={style.navCateMobile}>
-                    <div className={style.navCateHeaderMobile}>
-                        <div>
-                            <img src={logo} width={'50px'} style={{marginTop: '2px', marginLeft: '4px'}} />
-                        </div>
-                        <FontAwesomeIcon icon={faXmark} size="2xl" style={{color: '#fff', marginRight: '10px'}}/>
-                    </div>
-                    <div className={style.navContentMobile}>
-                        <div className={style.navIntroduceMobile}>
-                            <div className={style.navCateHeaderMobileTop}>
-                                <div className={style.navIntroduceRight}>
-                                    <span className={style.navIntroduceRightText}>Đăng nhập hoặc đăng ký để nhận thêm nhiều ưu đãi hấp dẫn</span>   
-                                    <button className={style.btnSignUpMobile}>Đăng ký</button>
-                                    <button className={style.btnSignInMobile}>Đăng nhập</button>
-                                </div>
-                                <div className={style.navIntroduceRight}>
-                                    <img src={loginMobile} width={'126px'}/>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div className={style.navItemCateMobileWrap}>
-                            <ul style={{padding:'0', margin:'0'}}>
-                                <li className={style.navItemCateMobile}>
-                                    <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
-                                </li>
-                                <li className={style.navItemCateMobile}>
-                                    <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className={style.navItemCateMobileWrap}>
-                            <ul style={{padding:'0', margin:'0'}}>
-                                <li className={style.navItemCateMobile}>
-                                    <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
-                                </li>
-                                <li className={style.navItemCateMobile}>
-                                    <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className={style.navItemCateMobileWrap}>
-                            <ul style={{padding:'0', margin:'0'}}>
-                                <li className={style.navItemCateMobile}>
-                                    <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
-                                </li>
-                                <li className={style.navItemCateMobile}>
-                                    <a className={style.navItemMobileTagA} href="">Icon và chữ</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>     
+                 
         </header>
     );
 };
